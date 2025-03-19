e88cb4db94eeede2605a795ffb271ccb7af68563
diboti <korposb@gmail.com>
2025-03-20 00:47:10 +0200

    second exc

--- 00-starting-js-proj/assets/scripts/app.js
+++ 00-starting-js-proj/assets/scripts/app.js
@@ -1,3 +1,11 @@
 function combine(a, b, c) {
-    return a * b / c;
+  return a * b / c;
+}
+
+function transformToObjects(numberArray) {
+  // Todo: Add your logic
+  // should return an array of objects
+  const transformedNumbersIntoObjects = numberArray.map((number) => ({val: number}));
+  
+  return transformedNumbersIntoObjects;
 }
