3306eac14ba2a81bb585366f2d18b96af2d7fb0b
diboti <korposb@gmail.com>
2025-03-20 20:06:27 +0200

    different syntaxes for props

--- Section_01/01-starting-project/src/App.jsx
+++ Section_01/01-starting-project/src/App.jsx
@@ -1,5 +1,6 @@
 import reactImg from "./assets/react-core-concepts.png"
 import componentsImg from "./assets/components.png"
+import { CORE_CONCEPTS } from "./data";
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
@@ -22,12 +23,12 @@ function Header() {
   );
 }
 
-function CoreConcept(props) {
+function CoreConcept({title, description, image} = props) {
   return (
     <li>
-      <img src={props.image} alt={props.title}/>
-      <h3>{props.title}/</h3>
-      <p>{props.description}/</p>
+      <img src={image} alt={title}/>
+      <h3>{title}</h3>
+      <p>{description}</p>
     </li>
   );
 }
@@ -41,13 +42,13 @@ function App() {
           <h2>Core concepts</h2>
           <ul>
             <CoreConcept
-              title="Components"
-              description="The core UI building block"
-              image={componentsImg}
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
             />
-            <CoreConcept />
-            <CoreConcept />
-            <CoreConcept />
+            <CoreConcept {...CORE_CONCEPTS[1]}/>
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/>
           </ul>
         </section>
         <h2>Time to get started!</h2>
