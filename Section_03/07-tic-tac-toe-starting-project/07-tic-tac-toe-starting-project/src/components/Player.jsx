a2140b87d91dfdd3005db18f9826eeb2cb816b40
diboti <korposb@gmail.com>
2025-03-24 00:05:57 +0200

    added logic to Player component

--- Section_03/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ Section_03/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -3,13 +3,21 @@ import { useState } from "react";
 export default function Player(props) {
   const [isEditing, setIsEditing] = useState(false);
 
+  function handleEditClick() {
+    setIsEditing(true);
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{props.name}</span>
+        {isEditing ? (
+          <input />
+        ) : (
+          <span className="player-name">{props.name}</span>
+        )}
         <span className="player-symbol">{props.symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>Edit</button>
     </li>
   );
 }
