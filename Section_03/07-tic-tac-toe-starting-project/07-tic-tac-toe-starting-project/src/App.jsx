73710d11a1dbf0f31c3d3ae7e9d26ed1b491b754
diboti <korposb@gmail.com>
2025-03-24 01:49:52 +0200

    upgraded game logic

--- Section_03/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/App.jsx
+++ Section_03/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/App.jsx
@@ -4,19 +4,27 @@ import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
 
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = "X";
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+
+  return currentPlayer;
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState("X");
+  // const [activePlayer, setActivePlayer] = useState("X");
+
+  const currentPlayer = deriveActivePlayer(gameTurns);
 
   function handleSelectedSquare(rowIndex, colIndex) {
-    setActivePlayer((currentlyActivePlayer) =>
-      currentlyActivePlayer === "X" ? "O" : "X"
-    );
+    // setActivePlayer((currentlyActivePlayer) =>
+    //   currentlyActivePlayer === "X" ? "O" : "X"
+    // );
     setGameTurns((prevTurns) => {
-      let currentPlayer = "X";
-      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
-        currentPlayer = "O";
-      }
+      const currentPlayer = deriveActivePlayer(prevTurns);
 
       const updatedTurns = [
         { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
@@ -34,12 +42,12 @@ function App() {
           <Player
             initialName="Player 1"
             symbol="X"
-            isActive={activePlayer === "X"}
+            isActive={currentPlayer === "X"}
           />
           <Player
             initialName="Player 2"
             symbol="O"
-            isActive={activePlayer === "O"}
+            isActive={currentPlayer === "O"}
           />
         </ol>
         <GameBoard onSelectedSquare={handleSelectedSquare} turns={gameTurns} />
--- Section_03/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ Section_03/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -39,6 +39,7 @@ export default function GameBoard(props) {
               <li key={colIndex}>
                 <button
                   onClick={() => props.onSelectedSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
                 >
                   {playerSymbol}
                 </button>
