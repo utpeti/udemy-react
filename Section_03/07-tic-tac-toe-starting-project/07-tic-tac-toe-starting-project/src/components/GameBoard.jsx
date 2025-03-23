17f3a69f5c1b4105c32a4f16c3c7e3c0b4659bdc
diboti <korposb@gmail.com>
2025-03-24 00:55:47 +0200

    added basic game logic

--- Section_03/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ Section_03/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,14 +7,30 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+  const [GameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectedSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const newGameBoard = [
+        ...prevGameBoard.map((innerArrays) => [...innerArrays]),
+      ];
+      newGameBoard[rowIndex][colIndex] = "X";
+      return newGameBoard;
+    });
+  }
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {GameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button
+                  onClick={() => handleSelectedSquare(rowIndex, colIndex)}
+                >
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
