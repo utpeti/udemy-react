52cfc9c5a74279dd0277e3412659de8eac266c07
diboti <korposb@gmail.com>
2025-03-31 00:08:42 +0200

    fixed all missing styles

--- Section_06/01-starting-project/01-starting-project/src/components/AuthInputs.jsx
+++ Section_06/01-starting-project/01-starting-project/src/components/AuthInputs.jsx
@@ -18,6 +18,17 @@ const Label = styled.label`
   color: #6b7280;
 `;
 
+const Input = styled.input`
+  width: 100%;
+  padding: 0.75rem 1rem;
+  line-height: 1.5;
+  background-color: #d1d5db;
+  color: #374151;
+  border: 1px solid transparent;
+  border-radius: 0.25rem;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+`;
+
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
   const [enteredPassword, setEnteredPassword] = useState("");
@@ -45,7 +56,7 @@ export default function AuthInputs() {
           <Label className={`label ${emailNotValid ? "invalid" : ""}`}>
             Email
           </Label>
-          <input
+          <Input
             type="email"
             //style={{ backgroundColor: emailNotValid ? "#fed2d2" : "d1d5db" }}
             className={emailNotValid ? "invalid" : undefined}
@@ -56,7 +67,7 @@ export default function AuthInputs() {
           <Label className={`label ${emailNotValid ? "invalid" : ""}`}>
             Password
           </Label>
-          <input
+          <Input
             type="password"
             style={{}}
             className={passwordNotValid ? "invalid" : undefined}
