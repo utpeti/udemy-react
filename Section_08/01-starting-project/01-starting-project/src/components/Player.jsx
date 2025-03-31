02d2bb37313d132dd2971d08d68dab12f0ed9ac0
diboti <korposb@gmail.com>
2025-04-01 00:13:17 +0200

    used useRef solution

--- Section_08/01-starting-project/01-starting-project/src/components/Player.jsx
+++ Section_08/01-starting-project/01-starting-project/src/components/Player.jsx
@@ -1,23 +1,18 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function Player() {
+  const playerNameInput = useRef(); //always a js obj -> current
   const [playerName, setPlayerName] = useState("");
-  const [submitted, setSubmitted] = useState(false);
-
-  function handleChange(event) {
-    setSubmitted(false);
-    setPlayerName(event.target.value);
-  }
 
   function handleClick() {
-    setSubmitted(true);
+    setPlayerName(playerNameInput.current.value);
   }
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? playerName : "unknown user"}</h2>
+      <h2>Welcome {playerName ?? "unknown user"}</h2>
       <p>
-        <input type="text" onChange={handleChange} value={playerName} />
+        <input ref={playerNameInput} type="text" />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
